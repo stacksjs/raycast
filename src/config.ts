b375dc4dc512c25d05ee5e7972b7437c4cf1bbe1
Chris <chrisbreuer93@gmail.com>
2025-04-07 14:35:42 -0700

    chore: wip

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
+// Local development API
 export const API_URL = 'http://raycast-api.test/api'
-// export const API_URL = "http://raycast-api.test/api";
+
+// For testing, using a mockable endpoint
+// export const API_URL = 'https://jsonplaceholder.typicode.com'
