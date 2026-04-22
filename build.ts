0d40717fd3f6acbf86eb3cc1a842a04cc96d1a93
chrisbreuer <chrisbreuer93@gmail.com>
2026-04-22 23:40:31 +0800

    chore: fix CI errors

--- build.ts
+++ build.ts
@@ -1,8 +1,9 @@
 import { dts } from 'bun-plugin-dtsx'
 
-// eslint-disable-next-line antfu/no-top-level-await
+// eslint-disable-next-line ts/no-top-level-await
 await Bun.build({
-  entrypoints: ['src/index.ts'],
+  entrypoints: ['src/index.tsx'],
   outdir: './dist',
+  external: ['@raycast/api', '@raycast/utils', 'react', 'react-dom'],
   plugins: [dts()],
 })
